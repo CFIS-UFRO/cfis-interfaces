616a89d05278528512850e22fb54bb2d26dd4e63
Bryan Casanelli <bryancasanelli@gmail.com>
2025-04-12 18:24:04 +0100

    Add publish.py to handle publishing new Python package versions.

---
+++ publish.py
@@ -0,0 +1,12 @@
+# Standard libraries
+import os
+# Third-party libraries
+from cfis_utils.PublishUtils import PublishUtils
+
+if __name__ == "__main__":
+    PublishUtils.publish_new_python_package_version(
+        toml_file_path=os.path.join(os.getcwd(), "pyproject.toml"),
+        readme_file_path=os.path.join(os.getcwd(), "README.md"),
+        repository_path=os.getcwd()
+    )
+    
